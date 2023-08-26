ae1853bea34881177957a75f488191c9ac92f229
VladimirDip <kent4594111@gmail.com>
2023-08-26 13:27:29 +1000

    test4

--- frontend/src/Auth.js
+++ frontend/src/Auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:4000";
+export const BASE_URL = "https://api.dip.nomoredomains.sbs";
 
 class AuthApi {
     getResponseData = (res) => {
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -94,7 +94,7 @@ export class Api {
 }
 
 const api = new Api({
-    baseUrl: 'http://localhost:4000',
+    baseUrl: 'https://api.dip.nomoredomains.sbs',
     headers: {
         "Accept": "application/json",
         "Content-Type": "application/json",
