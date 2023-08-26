37071f4fd6c7b3ddf2b3a9738596b6c1a97ba77e
VladimirDip <kent4594111@gmail.com>
2023-08-26 18:52:33 +1000

    fix: check new cors

--- backend/app.js
+++ backend/app.js
@@ -13,15 +13,7 @@ const serverError = require('./middlewares/serverError');
 
 const app = express();
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
-app.use(cors({
-  origin: [
-    'http://dip.nomoredomainsicu.ru',
-    'https://dip.nomoredomainsicu.ru',
-    'http://localhost:3000',
-    'https://localhost:3000',
-  ],
-  credentials: true,
-}));
+
 
 require('dotenv').config();
 
@@ -40,6 +32,15 @@ const limiter = rateLimit({
   standardHeaders: true,
   legacyHeaders: false,
 });
+app.use(cors({
+  origin: [
+    'http://dip.nomoredomainsicu.ru',
+    'https://dip.nomoredomainsicu.ru',
+    'http://localhost:3000',
+    'https://localhost:3000',
+  ],
+  credentials: true,
+}));
 app.use(requestLogger);
 app.use(limiter);
 app.use(routes);
