80f877240d0024c277ad9999abf94cfbd4b9db6d
VladimirDip <kent4594111@gmail.com>
2023-08-26 20:26:54 +1000

    test4

--- backend/app.js
+++ backend/app.js
@@ -5,8 +5,8 @@ const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
-const cors = require('cors');
-// const cors = require('./middlewares/cors');/**/
+// const cors = require('cors');
+const cors = require('./middlewares/cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const routes = require('./routes');
@@ -14,12 +14,7 @@ const serverError = require('./middlewares/serverError');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
-
-app.use(cors({
-  credentials: true,
-  maxAge: 15,
-  origins: ['https://localhost:3000', 'https://dip.nomoredomainsicu.ru']
-}));
+app.use(cors);
 
 app.use(helmet());
 app.use(cookieParser());
@@ -38,10 +33,9 @@ const limiter = rateLimit({
 app.use(requestLogger);
 app.use(limiter);
 app.use(routes);
-// app.use(cors);
-
 app.use(errorLogger);
 app.use(errors());
 app.use(serverError);
 
 app.listen(PORT);
+
--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,24 +1,27 @@
 const allowedCors = [
   'http://dip.nomoredomainsicu.ru',
   'https://dip.nomoredomainsicu.ru',
-  'http://localhost:3000',
-  'https://localhost:3000',
+  'http://localhost:3001',
+  'https://localhost:3001',
 ];
 
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 module.exports = (req, res, next) => {
+  console.log(req)
   const { origin } = req.headers;
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
-  res.header('Access-Control-Allow-Credentials', true);
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
   }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     res.end();
+    return
   }
   next();
 };
+
