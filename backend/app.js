bb4fee7b1a1c0a358e817af9500e30fbcf44d426
VladimirDip <kent4594111@gmail.com>
2023-08-26 19:01:50 +1000

    fix: check new cors

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,8 @@ const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
-const cors = require('cors');
-// const cors = require('./middlewares/cors');/**/
+// const cors = require('cors');
+const cors = require('./middlewares/cors');/**/
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const routes = require('./routes');
@@ -16,7 +16,6 @@ const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.en
 
 require('dotenv').config();
 
-// app.use(cors);
 app.use(helmet());
 app.use(cookieParser());
 app.use(express.json());
@@ -31,18 +30,20 @@ const limiter = rateLimit({
   standardHeaders: true,
   legacyHeaders: false,
 });
-app.use(cors({
-  origin: [
-    'http://dip.nomoredomainsicu.ru',
-    'https://dip.nomoredomainsicu.ru',
-    'http://localhost:3000',
-    'https://localhost:3000',
-  ],
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: [
+//     'http://dip.nomoredomainsicu.ru',
+//     'https://dip.nomoredomainsicu.ru',
+//     'http://localhost:3000',
+//     'https://localhost:3000',
+//   ],
+//   credentials: true,
+// }));
 app.use(requestLogger);
 app.use(limiter);
 app.use(routes);
+app.use(cors);
+
 app.use(errorLogger);
 app.use(errors());
 app.use(serverError);
