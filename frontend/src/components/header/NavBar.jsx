4bac0083f6d912b92236e1eb615c5fd7bcc241f4
VladimirDip <kent4594111@gmail.com>
2023-08-27 11:09:02 +1000

    fix: test

--- frontend/src/components/header/NavBar.jsx
+++ frontend/src/components/header/NavBar.jsx
@@ -2,6 +2,7 @@ import { Link, useLocation } from "react-router-dom";
 
 function NavBar({ loggedIn, email, signOut, menuIsOpen }) {
   const { pathname } = useLocation();
+  console.log(email)
   const textBar = pathname === `${"/sign-in"}` ? "Регистрация" : "Войти";
   const linkRoute = `${pathname === "/sign-in" ? "/sign-up" : "/sign-in"}`;
 
